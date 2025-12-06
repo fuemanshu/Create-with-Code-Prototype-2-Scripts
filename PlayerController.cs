4494db27f3c3a655c5a811cf48dbc060fed0e9e4
Fue Moua <fuemoua2022@gmail.com>
2025-12-05 22:01:09 -0600

    Game Boundary
    Added Boundary so player doesn't move off screen.

--- PlayerController.cs
+++ PlayerController.cs
@@ -8,6 +8,9 @@ public class PlayerController : MonoBehaviour
     // player movement speed
     public float playerSpeed = 10.0f;
 
+    // boundary variable
+    public float xRange = 10;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -19,8 +22,20 @@ public class PlayerController : MonoBehaviour
     {
         // move player left/right based on user input
         playerHorizontalInput = Input.GetAxis("Horizontal");
+
         // translate input to actually move player
         transform.Translate(Vector3.right * playerHorizontalInput * Time.deltaTime * playerSpeed);
+
+        // keep player inbounds
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
 
     }
 }
